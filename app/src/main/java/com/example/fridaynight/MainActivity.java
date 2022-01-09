10923075a408d320f7acda98a626f3046403052d
CodingAtMidnight <milkywaydebugger@gmail.com>
2022-01-09 07:32:27 -0500

    Fully incorporated onboarding screen.

--- app/src/main/java/com/example/fridaynight/MainActivity.java
+++ app/src/main/java/com/example/fridaynight/MainActivity.java
@@ -1,14 +1,128 @@
 package com.example.fridaynight;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.text.Html;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+
+    ViewPager mSlideViewPager;
+    LinearLayout mDotLayout;
+    Button backBtn, nextBtn, skipBtn;
+
+    TextView[] dots;
+    ViewPagerAdapter viewPagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        backBtn = findViewById(R.id.backBtn);
+        nextBtn = findViewById(R.id.nextBtn);
+        skipBtn = findViewById(R.id.skipButton);
+
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (getItem(0) > 0) {
+                    mSlideViewPager.setCurrentItem(getItem(-1), true);
+                }
+            }
+        });
+
+        nextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (getItem(0) < 3) {
+                    mSlideViewPager.setCurrentItem(getItem(1), true);
+                } else {
+                    Intent i = new Intent(MainActivity.this,mainscreen.class);
+                    startActivity(i);
+                    finish();
+                }
+            }
+        });
+
+        skipBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this,mainscreen.class);
+                startActivity(i);
+                finish();
+            }
+        });
+
+
+        mSlideViewPager = (ViewPager) findViewById(R.id.slideViewPager);
+        mDotLayout = (LinearLayout) findViewById(R.id.indicator_layout);
+
+        viewPagerAdapter = new ViewPagerAdapter(this);
+
+        mSlideViewPager.setAdapter(viewPagerAdapter);
+
+        setUpIndicator(0);
+        mSlideViewPager.addOnPageChangeListener(viewListener);
     }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public void setUpIndicator(int position) {
+        dots = new TextView[4];
+        mDotLayout.removeAllViews();
+
+        for (int i = 0; i < dots.length; i++) {
+            dots[i] = new TextView(this);
+            dots[i].setText(Html.fromHtml("&#8226"));
+            dots[i].setTextSize(35);
+            dots[i].setTextColor(getResources().getColor(R.color.inactive, getApplicationContext().getTheme()));
+            mDotLayout.addView(dots[i]);
+        }
+
+        dots[position].setTextColor(getResources().getColor(R.color.active, getApplicationContext().getTheme()));
+    }
+
+
+    ViewPager.OnPageChangeListener viewListener = new ViewPager.OnPageChangeListener() {
+
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+                setUpIndicator(position);
+
+                if (position > 0) {
+                    backBtn.setVisibility(View.VISIBLE);
+                } else {
+                    backBtn.setVisibility(View.INVISIBLE);
+                }
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
+    };
+
+    private int getItem(int i ) {
+
+        return mSlideViewPager.getCurrentItem() + i;
+    }
+
+
+
+
 }
